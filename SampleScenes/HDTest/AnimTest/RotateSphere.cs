28d5e645bf7c121f8b6a679efda10f1b805a1abe
sebastienlagarde <sebastien@unity3d.com>
2018-05-29 14:23:05 +0200

    Update anim test scene

---
+++ SampleScenes/HDTest/AnimTest/RotateSphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSphere : MonoBehaviour
+{
+    public float speed = 10f;
+    public bool flipRotateAxis = true;
+    public GameObject go; 
+
+    void Update()
+    {
+        if (go != null)
+        {
+            transform.RotateAround(go.transform.position, flipRotateAxis ? Vector3.up : Vector3.right, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        }
+    }
+}
